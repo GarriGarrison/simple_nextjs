61d7fa83db53632e79040a2c28dc0d1c22ac48f4
Garrison <garrison@pc103895.PKRVMN.local>
2022-09-21 13:20:43 +0300

    сверстали подвал и добавлен .env.example файл

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './Footer.props'
 import styles from './Footer.module.css'
 import cn from 'classnames'
+import {format} from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Garrison© – {format(new Date(), 'yyyy')}</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфидециальности
+      </a>
+    </footer>
+  )
 }
