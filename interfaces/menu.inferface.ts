e5fe90014ef4f41b84d3b44b1cf861bd12017a95
Garrison <garrison@pc103895.PKRVMN.local>
2022-09-23 16:48:38 +0300

    разработано меню приложения

--- interfaces/menu.inferface.ts
+++ interfaces/menu.inferface.ts
@@ -1,3 +1,5 @@
+import { TopLevelCategory } from './page.interface'
+
 // export interface Id {
 //   secondCategory: string
 // }
@@ -15,4 +17,12 @@ export interface MenuItem {
     secondCategory: string
   }
   pages: PageItem[]
+  isOpened?: boolean
+}
+
+export interface FirstLevelMenuItem {
+  route: string
+  name: string
+  icon: JSX.Element
+  id: TopLevelCategory
 }
